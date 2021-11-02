d2cff49dfe2a142456c56d43910a98b37dd69a12
Joshua Haughton <itsjoshuahaughton@gmail.com>
2021-11-02 17:02:50 +0000

    bug fixes

--- timer1.js
+++ timer1.js
@@ -1,20 +1,16 @@
 let args = process.argv.slice(2);
-let sec = 0
-for (let i = 0; i < args.length; i++) {
-  if (args[i] < 0) continue;
-  
 
-    sec = Number(args[i])
-  
+let delay = 0;
+let lastDelay = 0;
 
-  console.log(sec)
-  function helper(sec){  
+for (e of args) {
+  if (e < 0) continue;
+  lastDelay += delay;
+  delay = Number(e);
+  
     setTimeout(function() {
       process.stdout.write(`.`);
-    }, 1000*(sec))
-  }
-    helper(sec)
-  }
+    }, 1000*(delay + lastDelay))
   
-
+  }
   
